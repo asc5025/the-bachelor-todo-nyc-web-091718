bb9beaa61520e0c2c5315ed5a0df79390bac76ec
Albert Chun <albert.s.chun@gmail.com>
2018-09-12 15:46:22 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -17,6 +17,7 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
+  hometown_count = 0
   data.each do |season, contestants|
     contestants.each do |contestants_info|
       if contestants_info["hometown"] == hometown
